894d121e56fdfa21119b7e1831fc975d5986b90c
Michel Floyd <michel.floyd@kaazing.com>
2016-03-24 15:32:16 -0700

    Tweaked code indentation

--- javascript-starter-app/index.js
+++ javascript-starter-app/index.js
@@ -3,14 +3,14 @@ var messageCounter = 1;
 var client = UniversalClientDef("amqp");
 var connectionInfo = {
 	url: "ws://localhost:8001/amqp",// URL to connect
-	username: "guest",// User name
-	password: "guest"// User password
+	username: "guest", // User name
+	password: "guest"  // User password
 };
 var TOPIC_PUB="websocket-starter";
-var	TOPIC_SUB="websocket-starter";
+var TOPIC_SUB="websocket-starter";
 var subscription={};
 
-var onMessage=function(msg) {
+var onMessage=function(msg) { // callback function to process received message
 	console.log("Received from the server: " + msg.message);
 	var text=$('#server-data').html().trim();
 	if (text.length!=0){
@@ -24,20 +24,20 @@ var onError=function(err) {
 };
 
 client.connect(connectionInfo, // Connection info
-				onError, // callback function to process errors
-				function(connection){
-					connection.subscribe(TOPIC_PUB, // Topic to send message
-										 TOPIC_SUB, // Topic to subscribe to receive messsages
-											onMessage, // callback function to process received message
-											false, // noLocal flag set to false - allow receiving your own messages
-						function(subscr){
-							console.info("Subscription is created "+subscr);
-							subscription=subscr;
-							subscription.sendMessage({message:"From "+clientID+": Initial message is sent!"});
-							$('#messageToSend').val("Message " + messageCounter + " is sent!");
-						});
-					}
-				);
+	       onError, // callback function to process errors
+	       function(connection){
+			connection.subscribe(TOPIC_PUB, // Topic to send message
+			TOPIC_SUB, // Topic to subscribe to receive messsages
+			onMessage, // callback function to process received message
+			false, // noLocal flag set to false - allow receiving your own messages
+			function(subscr){
+				console.info("Subscription is created "+subscr);
+				subscription=subscr;
+				subscription.sendMessage({message:"From "+clientID+": Initial message is sent!"});
+				$('#messageToSend').val("Message " + messageCounter + " is sent!");
+			});
+		}
+);
 $( window ).unload(function() {
 	// TODO: Disconnect
 	client.close();
